160be1d87053ec902a336c510933cb4d4437497c
Marten van Schijndel <vanschm@ling.osu.edu>
2014-11-09 20:49:33 -0500

    simeval: added some features to tree library that could be useful based on planned directions;

--- scripts/tree.py
+++ scripts/tree.py
@@ -6,8 +6,20 @@ class Tree(object):
     else:
       self.valence = 0
     self.data = None
+    self.depth = None #depth is counted from the leaves, so it's a measure of abstractness
+    self.head = None
 
   def sprout(self, numkids):
     #grow [numkids] child tuple nodes
     self.children = (Tree(),)*numkids
     self.valence = numkids
+
+  def getDepth(self):
+    #return the depth of the current node
+    if not self.depth:
+      self.depth = max(ch.getDepth() for ch in self.children) + 1
+    return(self.depth)
+
+  def setHead(self, head):
+    #tells the node the index of the child that heads it
+    self.Head = head
